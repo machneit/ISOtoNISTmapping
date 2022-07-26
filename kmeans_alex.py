d9c0298e3ec8af432f9c32979b3b137ef875aa7e
Alexandre Giard <59082879+machneit@users.noreply.github.com>
2022-07-26 00:03:45 -0400

    Add files via upload

--- kmeans_alex.py
+++ kmeans_alex.py
@@ -22,38 +22,43 @@ st = LancasterStemmer()
 
 
 # Get the data from the stardards formatted in YAML files
-def get_data(file_name):
+def get_data(file_name, type):
     with open(file_name) as file:
         liste_normes = yaml.load(file, Loader=yaml.FullLoader)  # Structure : [{id,name,text},{id...}]
         texts = []
         titles = []
         ids = []
-        TITLE_WEIGHTING = 7  # Keywords in the title are X times more important than those in the text
+        ISO_TITLE_WEIGHTING = 8  # Keywords in the title are X times more important than those in the text
+        NIST_TITLE_WEIGHTING = 5
         for norme in liste_normes:
             formatted_title = " ".join([st.stem(w) for w in word_tokenize(norme['name']) if w not in stop_words])
-            texts.append((formatted_title + ' ')*TITLE_WEIGHTING
-                         + norme['text'][:1000])  # Cap text size at 1000 characters
+            if type == 'ISO':
+                texts.append((formatted_title + ' ') * ISO_TITLE_WEIGHTING
+                             + norme['text'][:1000])  # Cap text size at 1000 characters
+            else:
+                texts.append(((formatted_title + ' ') * NIST_TITLE_WEIGHTING
+                             + norme['text'][:1]).replace('cybersec', 'sec'))  # Cap text size at 1000 characters
             ids.append(norme['id'])
             titles.append(norme['name'])
         return texts, ids, titles
 
 
 # Retreive data
-iso_texts, iso_ids, iso_titles = get_data('ISO_standards.yml')
-nist_texts, nist_ids, nist_titles = get_data('NIST_standards.yml')
+iso_texts, iso_ids, iso_titles = get_data('ISO_standards.yml', 'ISO')
+nist_texts, nist_ids, nist_titles = get_data('NIST_standards.yml', 'NIST')
 clusters = []  # List of tuples : [C1(standards, features), (standards, features)]
 
 for k in range(len(iso_texts)):
     texts, ids = [iso_texts[k]] + nist_texts, [iso_ids[k]] + nist_ids
 
     # TF-IDF considering unigrams and bigrams
-    cv = CountVectorizer(ngram_range=(1, 2), min_df=0.001, max_df=0.75)  # max_df set to eliminate most common words (like 'sec')
+    cv = CountVectorizer(ngram_range=(1, 2), min_df=0.001, max_df=0.15)  # max_df set to eliminate most common words (like 'sec')
     cv_matrix = cv.fit_transform(texts).todense()
 
     # Create Kmeans model
-    NUM_CLUSTERS, MIN_CLUSTER_SIZE, MAX_CLUSTER_SIZE = 40, 2, 4
-    model = KMeansConstrained(n_clusters=NUM_CLUSTERS, size_min=MIN_CLUSTER_SIZE, size_max=MAX_CLUSTER_SIZE, max_iter=10000,
-                              n_init=100, random_state=42)  # Initializing KMeans model
+    NUM_CLUSTERS, MIN_CLUSTER_SIZE, MAX_CLUSTER_SIZE = 28, 2, 4
+    model = KMeansConstrained(n_clusters=NUM_CLUSTERS, size_min=MIN_CLUSTER_SIZE, size_max=MAX_CLUSTER_SIZE, max_iter=1000,
+                              n_init=50)  # Initializing KMeans model
     km = model.fit(cv_matrix)  # Applying it to our data
 
     # Get top 4 key features for each cluster
@@ -76,7 +81,7 @@ for k in range(len(iso_texts)):
                 break
 
 header = ['ISO27002 standards', 'NIST Cybersecurity Framework standards', 'Common keywords']
-with open('mapping.csv', 'w', encoding='UTF8') as f:
+with open('mapping2.csv', 'w', encoding='UTF8') as f:
     writer = csv.writer(f, delimiter=";", lineterminator='\n')
     writer.writerow(header)  # write the header
     for i in range(len(iso_ids)):  # write the data
